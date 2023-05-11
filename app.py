293619f54cb9ea7f7ca1ec2f4615907cbfcd63d0
Oamen_Modupe <84418297+NeonStone7@users.noreply.github.com>
2023-05-11 19:39:03 +0100

    Add files via upload

---
+++ app.py
@@ -0,0 +1,28 @@
+import Flask
+from flask import Flask, jsonify, request
+import numpy as np
+from sklearn.externals import joblib
+import pandas as pd
+import numpy as np
+
+app = Flask(__name__)
+
+@app.route('/index')
+def index():
+    return flask.render_template('index.html')
+
+@app.route('/predict',methods = ['POST'])
+def predict():
+    model = joblib.load('model_text_class.joblib')
+    to_predict=request.form.to_dict()
+    review_text = to_predict_list['review_text']
+    prediction = model.predict([review_text])
+    
+    if prediction[0]:
+        prediction = 'Not COVID-19 related'
+    else:
+        prediction = 'COVID-19 related'
+        
+    return jsonify({'prediction':prediction})
+if __name__ = '__main__':
+    app.run(host='0.0.0.0', port = 8080)
